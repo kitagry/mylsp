81e45b81ac27d16b81557c275078732cd8d28ef7
Ryo Kitagawa <kitadrum50@gmail.com>
2024-05-05 15:51:48 +0900

    feat: add text document sync support

---
+++ go.mod
@@ -0,0 +1,8 @@
+module mylsp
+
+go 1.22.2
+
+require (
+	github.com/sourcegraph/go-lsp v0.0.0-20240223163137-f80c5dd31dfd
+	github.com/sourcegraph/jsonrpc2 v0.2.0
+)
---
+++ langserver/handler.go
@@ -0,0 +1,46 @@
+package langserver
+
+import (
+	"context"
+	"errors"
+
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+type handler struct {
+	conn  *jsonrpc2.Conn
+	cache *cache
+}
+
+func NewHandler() jsonrpc2.Handler {
+	h := &handler{
+		cache: newCache(),
+	}
+	return jsonrpc2.HandlerWithError(h.handle)
+}
+
+func (h *handler) handle(
+	ctx context.Context,
+	conn *jsonrpc2.Conn,
+	req *jsonrpc2.Request,
+) (result any, err error) {
+	if req.Params == nil {
+		return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+	}
+
+	switch req.Method {
+	case "initialize":
+		return h.handleInitialize(ctx, conn, req)
+	case "initialized":
+		return nil, nil
+	case "textDocument/didOpen":
+		return h.handleTextDocumentDidOpen(ctx, conn, req)
+	case "textDocument/didChange":
+		return h.handleTextDocumentDidChange(ctx, conn, req)
+	case "textDocument/didClose":
+		return h.handleTextDocumentDidClose(ctx, conn, req)
+	case "textDocument/disSave":
+		return h.handleTextDocumentDidSave(ctx, conn, req)
+	}
+	return nil, errors.New("not implemented")
+}
